d5edd234c45c604f92c020eaea1291a9247158dd
Anik Gupta <85663156+xlrboi@users.noreply.github.com>
2026-09-01 03:58:37 +0530

    Time: 11 ms (55.03%), Space: 20.1 MB (88.58%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -5,7 +5,7 @@ class Solution:
         for ch in s:
             dict1[ch] = dict1.get(ch, 0) + 1
 
-        sorted_char = sorted(dict1.items(), key = lambda x : x[1], reverse = True)
+        sorted_char = sorted(dict1.items(), key = lambda x : (-x[1], x[0]))
         for char, freq in sorted_char:
             result += (char * freq)
 
