ec10544dca734055b589f86248746d3acf19de1f
Anik Gupta <85663156+xlrboi@users.noreply.github.com>
2026-09-04 00:01:10 +0530

    Time: 2873 ms (62.38%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,2 +1,5 @@
 /* Write your T-SQL query statement below */
-select p.product_name, s.year, s.price from Product as p inner join Sales as s on p.product_id = s.product_id
+SELECT P.product_name, S.year, S.price
+FROM Sales AS S
+LEFT JOIN Product AS P
+ON S.product_id = P.product_id
