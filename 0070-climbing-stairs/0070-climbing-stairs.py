872318a6be769145b2f7f3e8bdb314e22afd14b6
Anik Gupta <85663156+xlrboi@users.noreply.github.com>
2026-09-17 12:06:26 +0530

    Time: 0 ms (100%), Space: 19.2 MB (87.17%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,15 +1,10 @@
 class Solution:
-    def func(self, index, dp):
-        if index == 0 or index == 1:
-            return 1
-
-        if dp[index] != -1:
-            return dp[index]
-
-        dp[index] = self.func(index - 1, dp) + self.func(index - 2, dp)
-        return dp[index]
 
     def climbStairs(self, n: int) -> int:
-         dp = [-1] * (n + 1)
-         return self.func(n, dp)
+        dp = [-1] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
 
+        return dp[n]
