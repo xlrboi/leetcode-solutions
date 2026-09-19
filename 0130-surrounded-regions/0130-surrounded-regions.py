bb2a34d22095e0c40cfe46b7b6cc63df7fbebf17
Anik Gupta <85663156+xlrboi@users.noreply.github.com>
2026-09-19 08:07:24 +0530

    Time: 6 ms (56.98%), Space: 22.2 MB (91.54%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -21,13 +21,32 @@ class Solution:
         rows = len(board)
         cols = len(board[0])
         visited = [[0 for _ in range(cols)]for _ in range(rows)]
-        for r in range(rows):
-            for c in range(cols):
-                if r == 0 or c == 0 or r == rows - 1 or c == cols - 1:
-                    if board[r][c] == "O":
+        r, c = 0, 0 
+        for c in range(cols):
+            if board[r][c] == "O":
                         if visited[r][c] == 0:
                             self.dfs(r, c, visited, rows, cols, board)
 
+        r, c = rows - 1, 0
+        for c in range(cols):
+            if board[r][c] == "O":
+                        if visited[r][c] == 0:
+                            self.dfs(r, c, visited, rows, cols, board)
+
+        r, c = 0, 0
+        for r in range(rows): 
+            if board[r][c] == "O":
+                        if visited[r][c] == 0:
+                            self.dfs(r, c, visited, rows, cols, board)
+
+        r, c = 0, cols - 1
+        for r in range(rows): 
+            if board[r][c] == "O":
+                        if visited[r][c] == 0:
+                            self.dfs(r, c, visited, rows, cols, board)
+
+
+        
         for r in range(rows):
             for c in range(cols):
                 if board[r][c] == "O" and visited[r][c] == 0:
